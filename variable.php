52d8bf00a8f49b72c1d655ccc5f6bf2e6761ebd3
RVIRUS0817 <t.virus.1989@gmail.com>
2019-06-08 15:07:37 +0900

    add variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+  変数: データにつけるラベル
+
+  データ型:
+  - 文字列型 string
+  - 数値 integer float
+  - 論理値 boolean / true false
+  - 配列
+  - オブジェクト
+  - null
+
+ */
+
+  $msg = "hello adachinpo";
+  echo $msg;
+  var_dump($msg);
+
+?>
+
