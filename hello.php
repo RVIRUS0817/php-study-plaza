a4fd7ff7a0b2072098a237036375c2379399c756
RVIRUS0817 <t.virus.1989@gmail.com>
2019-06-08 14:21:30 +0900

    add hello.php

---
+++ hello.php
@@ -0,0 +1,19 @@
+<?php
+
+// comment
+# comment
+/*
+ comment
+ */
+
+echo "hello adachinpo";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+<p>adachinpo <?php echo " from PHP"; ?></p>
+</body>
+</html>
+
