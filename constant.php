1a87e0c244f565070b394764238948f984715e0b
RVIRUS0817 <t.virus.1989@gmail.com>
2019-06-08 15:15:02 +0900

    add constant.php

---
+++ constant.php
@@ -0,0 +1,16 @@
+<?php
+
+// 定数: 変更されない値につけるラベル
+
+
+define("MY_EMAIL", "t.virus.1989@gmail.com");
+
+echo MY_EMAIL;
+// MY_EMAIL = "hoge";
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
+
+?>
+
